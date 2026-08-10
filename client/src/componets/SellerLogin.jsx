2207c24e6b32ebb3daff63708261d813e42ab4d9
Rahulvsk <rahulvsk2456@gmail.com>
2026-08-10 23:21:40 +0530

    seller acount done

--- client/src/componets/SellerLogin.jsx
+++ client/src/componets/SellerLogin.jsx
@@ -1,14 +1,40 @@
-import React, { useState } from 'react'
+import  { useEffect, useState } from 'react'
 import { useAppContext } from '../context/AppContext'
 
 const SellerLogin = () => {
     const {navigate,setIsSeller,isSeller} = useAppContext()
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
-  return (
-    <div>
+    
 
-    </div>
+    const onSubmitHandler = async (event)=>{
+      event.preventDefault();
+      setIsSeller(true)
+    }
+  
+
+    useEffect(()=>{
+      if(isSeller){
+        navigate("/seller")
+      }
+    },[isSeller])
+
+
+  return !isSeller && (
+    <form onSubmit={onSubmitHandler} className='min-h-screen flex items-center text-sm text-gray-600'>
+      <div className='flex flex-col  gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200'>
+        <p className='text-2xl font-medium m-auto'><span className='text-green-600'>Seller</span> Login</p>
+        <div className='w-full'>
+          <p>Email</p>
+          <input onChange={(e)=> setEmail(e.target.value)} value={email} type="email" className='border border-gray-200 rounded w-full p-2 mt-1 outline-green-600' placeholder='Eneter Your Email' required/>
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+          <input type="password" onChange={(e)=> setPassword(e.target.value)} value={password} className='border border-gray-200 rounded w-full p-2 mt-1 outline-green-600' placeholder='Eneter Your Password' required/>
+        </div>
+        <button className='bg-green-600 text-white w-full py-2 rounded-md cursor-pointer'>Login</button>
+      </div>
+    </form>
   )
 }
 
